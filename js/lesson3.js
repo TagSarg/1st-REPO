927606fe3ea55c006f28bf4ea80e3759cc687802
TagSarg <71183359+TagSarg@users.noreply.github.com>
2020-09-15 01:41:23 +0400

    Create lesson3.js

---
+++ js/lesson3.js
@@ -0,0 +1,7 @@
+const a = Math.PI;
+let r = prompt("Enter the radius of the circle");
+let s = a*r*r;
+let p = 2*a*r;
+console.log("the area of the circle is:  " +s);
+console.log("the perimeter of the circle is:  " +s);
+alert("Circle:  Radius = "+r+"  Area = "+s+"  Perimeter = " +p);  
